3f1d2169f778f40bf9e6c04ba28aaa9326c2cb2a
Evgeny Proff <32987246+EvgenyProff@users.noreply.github.com>
2018-03-15 00:49:33 +0800

    Add files via upload

---
+++ beta/backup/unit3.pas
@@ -0,0 +1,30 @@
+unit Unit3;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, RichMemo;
+
+type
+
+  { TForm3 }
+
+  TForm3 = class(TForm)
+    RichMemo1: TRichMemo;
+  private
+
+  public
+
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
